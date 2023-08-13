5c3cef7677d18dd530997c3f0bbc4e754754e8d1
= <dante.clericuzio@fintopia.tech>
2023-08-13 18:01:14 +0700

    revise head ico

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel='icon' href='./arrow.png' />
+        <link rel='icon' href='/arrow.ico' />
       </head>
       <body className={`${dmsans.className}`}>
         <header>
