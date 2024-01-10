a94a718a23982904395d85f7a6e3d154cbda3eaa
= <dante.clericuzio@fintopia.tech>
2024-01-10 16:14:09 +0700

    add change theme feature: home done, nav n footer

--- components/layout/navigationbar.js
+++ components/layout/navigationbar.js
@@ -2,17 +2,18 @@
 import Link from 'next/link'
 import React, {useState} from 'react'
 import { links } from '@/app/constants'
+import ThemeSwitcher from '@/components/ThemeSwitcher'
 
 export default function navigationbar() {
 
   const [open, setOpen] = useState(false)
 
   return (
-    <nav className='bg-[#1C1C1C] py-5 px-[16px] z-50 fixed top-0 w-full'>
+    <nav className='bg-[#E4E4E4] dark:bg-[#1C1C1C] py-5 px-[16px] z-50 fixed top-0 w-full'>
       
       <div className='flex justify-between items-center dc-container'>
         <div className='md:flex hidden items-center'>
-          <Link href='/' className='tracking-[-2px] text-[28px] font-[700] text-[#68CC93]'>Dante Clericuzio</Link>
+          <Link href='/' className='tracking-[-2px] text-[28px] font-[700] text-[#1C1C1C] dark:text-[#68CC93]'>Dante Clericuzio</Link>
           <img src='./danteclericuzio.png' alt='logo' className='hidden lg:block w-[50px] h-[50px] bg-contain bg-center rounded-full bg-no-repeat ml-[16px]'/>
         </div>
 
@@ -24,7 +25,7 @@ export default function navigationbar() {
               (
                 <Link
                   key={i}
-                  className='text-[#E4E4E4] hover:text-[#68CC93] mr-[40px] text-[24px] font-[700] animation-effect'
+                  className='text-[#1C1C1C] dark:text-[#E4E4E4] hover:text-[#FF5733] dark:hover:text-[#68CC93] mr-[40px] text-[24px] font-[700] animation-effect'
                   href={item.href}
                   {...(i === links.length - 2 ? { target: '_blank', rel: 'noopener noreferrer' } : {})}
                 >
@@ -33,14 +34,18 @@ export default function navigationbar() {
               )
               :
               (
+                <div className='flex flex-row items-center'>
                   <Link 
                     key={i}
                     href={item.href}
                   >
-                    <button className='hidden lg:block rounded-md py-[12px] px-[24px] text-[#E4E4E4] hover:text-[#FCFAF4] border-border border border-[#3E3E3E] hover:border-[#68CC93] hover:bg-[#68CC93] animation-effect'>
+                    <button className='mr-[20px] hidden lg:block rounded-md py-[12px] px-[24px] text-[#1C1C1C] hover:text-[#FCFAF4] dark:text-[#E4E4E4] dark:hover:text-[#FCFAF4] border-border border border-[#3E3E3E] hover:border-[#FF5733] hover:bg-[#FF5733] dark:hover:border-[#68CC93] dark:hover:bg-[#68CC93] animation-effect'>
                       {item.label}
                     </button>
                   </Link>
+                  <ThemeSwitcher/>
+                </div>
+                  
               )
             ))}
           </ul>
@@ -48,17 +53,17 @@ export default function navigationbar() {
 
         {/* HAMBURGER */}
         <div className='flex ml-auto md:hidden group z-50 w-6 h-6 cursor-pointer flex-col justify-between items-center' onClick={() => { setOpen(!open) }}>
-          <span className={`h-1 w-full rounded-lg cursor-pointer transform transition duration-300 ease-in-out bg-[#68CC93] ${open ? "rotate-45 translate-y-2.5" : ""}`} />
-          <span className={`h-1 rounded-lg cursor-pointer transition-all duration-300 ease-in-out bg-[#E4E4E4] ${open ? "w-0" : "w-full"}`} />
-          <span className={`h-1 w-full rounded-lg cursor-pointer transform transition duration-300 ease-in-out bg-[#68CC93] ${open ? "-rotate-45 -translate-y-2.5" : ""}`} />
+          <span className={`h-1 w-full rounded-lg cursor-pointer transform transition duration-300 ease-in-out bg-[#FF5733] dark:bg-[#68CC93] ${open ? "rotate-45 translate-y-2.5" : ""}`} />
+          <span className={`h-1 rounded-lg cursor-pointer transition-all duration-300 ease-in-out bg-[#1C1C1C] dark:bg-[#E4E4E4] ${open ? "w-0" : "w-full"}`} />
+          <span className={`h-1 w-full rounded-lg cursor-pointer transform transition duration-300 ease-in-out bg-[#FF5733] dark:bg-[#68CC93] ${open ? "-rotate-45 -translate-y-2.5" : ""}`} />
         </div>
       </div>
 
       {/* MOBILE */}
-      <div className={`${open ? 'max-h-full' : 'max-h-0 overflow-hidden'} px-[16px] bg-[#1C1C1C] fixed top-0 left-0 z-10 w-full h-full animation-effect`}>
+      <div className={`${open ? 'max-h-full' : 'max-h-0 overflow-hidden'} px-[16px] bg-[#E4E4E4] dark:bg-[#1C1C1C] fixed top-0 left-0 z-10 w-full h-full animation-effect`}>
         <ul className='flex flex-col w-full h-full mt-36'>
           <div onClick={() => { setOpen(!open) }} className='flex items-center mb-[40px]'>
-            <Link href='/' className='tracking-[-2px] text-[28px] font-[700] text-[#68CC93]'>Dante Clericuzio</Link>
+            <Link href='/' className='tracking-[-2px] text-[28px] font-[700] text-[#1C1C1C] dark:text-[#68CC93]'>Dante Clericuzio</Link>
             <img src='./danteclericuzio.png' alt='logo' className='w-[50px] h-[50px] bg-contain bg-center rounded-full bg-no-repeat ml-[16px]'/>
           </div>
           {links.map((item, i)=>(
@@ -66,7 +71,8 @@ export default function navigationbar() {
             ? 
             (
               <Link 
-                className='text-[#E4E4E4] hover:text-[#68CC93] mb-[40px] text-[24px] font-[700] animation-effect'
+                key={i}
+                className='text-[#1C1C1C] dark:text-[#E4E4E4] hover:text-[#FF5733] dark:hover:text-[#68CC93] mb-[40px] text-[24px] font-[700] animation-effect'
                 href={item.href}
                 onClick={() => { setOpen(!open) }}
                 {...(i === links.length - 2 ? { target: '_blank', rel: 'noopener noreferrer' } : {})}
@@ -81,7 +87,7 @@ export default function navigationbar() {
                 href={item.href} 
                 onClick={() => { setOpen(!open) }}
               >
-                <button className='w-fit rounded-[10px] py-[12px] px-[24px] text-[#E4E4E4] hover:text-[#FCFAF4] border-[1px] border-[#E4E4E4] hover:border-[#68CC93] hover:bg-[#68CC93] animation-effect'>
+                <button className='w-fit rounded-[10px] py-[12px] px-[24px] text-[#1C1C1C] hover:text-[#FCFAF4] dark:text-[#E4E4E4] dark:hover:text-[#FCFAF4] border-[1px] border-[#3E3E3E] hover:border-[#FF5733] hover:bg-[#FF5733] dark:hover:border-[#68CC93] dark:hover:bg-[#68CC93] animation-effect'>
                   {item.label}
                 </button>
               </Link>
--- components/organism/HomeHero/index.js
+++ components/organism/HomeHero/index.js
@@ -7,10 +7,10 @@ import { svgLogos } from "@/app/constants"
 
 export default function HomeHero(){
     return (
-        <div className="bg-[#1C1C1C] min-h-screen pt-20 pb-5 px-[16px]">
+        <div className="bg-[#E4E4E4] dark:bg-[#1C1C1C] min-h-screen pt-20 pb-5 px-[16px]">
             <div className="dc-container">
                 
-                <div className="text-[#68CC93] text-center title-responsive font-[600] whitespace-nowrap tracking-[-1px] md:tracking-[-3px]">
+                <div className="text-[#1C1C1C] dark:text-[#68CC93] text-center title-responsive font-[600] whitespace-nowrap tracking-[-1px] md:tracking-[-3px]">
                     <Typewriter
                     options={{
                         strings: ['Web Developer </>','Software Engineer ⚙︎'],
@@ -25,13 +25,13 @@ export default function HomeHero(){
                     {/* 1st */} 
                     {/* testing */}
                     <Link 
-                    href='/about'
+                        href='/about'
                     >
-                    <div className="card1 animation-effect flex items-center group">
+                    <div className="bg-[#F9F9F9] hover:bg-[#FF5733] dark:bg-[#2C2C2C] dark:hover:bg-[#68CC93] card1 animation-effect flex items-center group">
                         <div className="flex flex-col relative w-full">
-                        <h1 className="md:group-hover:text-transparent group-hover:text-[#68CC93] font-[700] text-[#E4E4E4]  text-[40px] tracking-[-2px] md:group-hover:-translate-y-6 animation-effect">Hey, I'm Dante</h1>
+                        <h1 className="md:group-hover:text-transparent font-[700] text-[#1C1C1C] group-hover:text-[#E4E4E4] dark:text-[#E4E4E4]  text-[40px] tracking-[-2px] md:group-hover:-translate-y-6 animation-effect">Hey, I'm Dante</h1>
                         <div className="justify-between flex items-center md:group-hover:translate-y-6 animation-effect">
-                            <h1 className="hidden md:block mr-5 md:group-hover:text-[#68CC93] font-[500] text-[#7E7E7E] text-[20px] tracking-[-1px] animation-effect">"Website without visitors is like a ship lost in the horizon."</h1>
+                            <h1 className="hidden md:block mr-5 md:group-hover:text-transparent font-[500] text-[#7E7E7E] text-[20px] tracking-[-1px] animation-effect">"Website without visitors is like a ship lost in the horizon."</h1>
                             <h1 className="block group-hover:text-[#E4E4E4] md:hidden mr-5 font-[500] text-[20px] tracking-[-1px] animation-effect">Learn More About Me</h1>
                             <div className="group-hover:border-[#E4E4E4] button-arrow">
                             <img src="./arrow.png" alt="arrow" className="group-hover:rotate-90 animation-effect"/>
@@ -47,11 +47,11 @@ export default function HomeHero(){
                     href='https://www.easycash.id/' 
                     target="_blank" rel="noopener noreferrer"
                     >
-                    <div className="card1 animation-effect group flex overflow-hidden">
+                    <div className="bg-[#F9F9F9] hover:bg-[#FF5733] dark:bg-[#2C2C2C] dark:hover:bg-[#68CC93] card1 animation-effect group flex overflow-hidden">
                         <div className="flex flex-col relative w-full">
                         <div className="justify-between flex items-center group-hover:translate-y-6 animation-effect">
                             <div className="flex flex-col">
-                            <h1 className="mr-5 group-hover:text-transparent font-[500] text-[#E4E4E4] text-[14px] tracking-[-1px] animation-effect">Corporate Website</h1>
+                            <h1 className="mr-5 group-hover:text-transparent font-[500] text-[#1C1C1C] dark:text-[#E4E4E4] text-[14px] tracking-[-1px] animation-effect">Corporate Website</h1>
                             <h1 className="mr-5 group-hover:text-transparent font-[500] text-[#7E7E7E] text-[28px] tracking-[-1px] animation-effect">Easycash</h1>
                             </div>
                             <div className="flex flex-col absolute">
@@ -70,11 +70,11 @@ export default function HomeHero(){
                     {/* 3rd & 4th */}
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
                     <Link href='/works'>
-                        <div className="card1 group flex overflow-hidden animation-effect">
+                        <div className="bg-[#F9F9F9] hover:bg-[#FF5733] dark:bg-[#2C2C2C] dark:hover:bg-[#68CC93] card1 group flex overflow-hidden animation-effect">
                         <div className="flex flex-col justify-center relative w-full">
                             <BsStack className="text-[#7E7E7E] mt-10 group-hover:hidden w-[75px] h-[75px] mx-auto lg:mx-0"/>
                             <div className="justify-between flex items-center ">
-                            <h1 className="mx-auto lg:mx-0 font-[500] text-[#E4E4E4] text-[28px] tracking-[-1px] group-hover:text-transparent animation-effect">See My Work</h1>
+                            <h1 className="mx-auto lg:mx-0 font-[500] text-[#1C1C1C] dark:text-[#E4E4E4] text-[28px] tracking-[-1px] group-hover:text-transparent animation-effect">See My Work</h1>
                             <div className="absolute bottom-[12px] right-0 group-hover:border-[#E4E4E4] button-arrow">
                                 <img src="./arrow.png" alt="arrow" className="group-hover:rotate-90 animation-effect"/>
                             </div>
@@ -85,7 +85,7 @@ export default function HomeHero(){
                     </Link>
 
                     <Link href="https://www.linkedin.com/in/dante-clericuzio-1b9345241/" target="_blank" rel="noopener noreferrer">
-                        <div className="card1 group flex overflow-hidden animation-effect">
+                        <div className="bg-[#F9F9F9] hover:bg-[#FF5733] dark:bg-[#2C2C2C] dark:hover:bg-[#68CC93] card1 group flex overflow-hidden animation-effect">
                         <div className="flex flex-col justify-center relative w-full">
                             <FaLinkedin className="group-hover:text-[#E4E4E4] text-[#7E7E7E] text-[100px] mx-auto group-hover:mb-5 group-hover:scale-125 animation-effect"/>
                             <h1 className="whitespace-nowrap absolute bottom-[12px] left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-[500] text-transparent text-[28px] tracking-[-1px] group-hover:text-[#E4E4E4] animation-effect">Let's connect</h1>
@@ -98,9 +98,9 @@ export default function HomeHero(){
                     </div>
 
                     <Link href='/contact'>
-                    <div className="card1 group flex overflow-hidden animation-effect">
+                    <div className="bg-[#F9F9F9] hover:bg-[#FF5733] dark:bg-[#2C2C2C] dark:hover:bg-[#68CC93] card1 group flex overflow-hidden animation-effect">
                         <div className="flex flex-col relative w-full">
-                        <h1 className="mr-5 font-[500] text-[#E4E4E4] text-[14px] tracking-[-1px]">WHAT I USE</h1>
+                        <h1 className="mr-5 font-[500] text-[#1C1C1C] dark:text-[#E4E4E4] group-hover:text-[#E4E4E4] text-[14px] tracking-[-1px]">WHAT I USE</h1>
                         <div className="">
                             <div className='logos'>
                             <div className='logos-slide'>
